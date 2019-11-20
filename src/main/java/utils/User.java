8df70577330c276f4d6eb47bd6c8bb6e76d5ae3d
HanhNguyenDuc <nguyenduchanh99tnvn@gmail.com>
2019-11-20 17:07:44 +0700

    user class

--- src/main/java/utils/User.java
+++ src/main/java/utils/User.java
@@ -10,3 +10,26 @@ package utils;
  * @author ADMIN
  */
 
+class User{
+    private String username;
+
+    public User(String username, String password, int user_id) {
+        this.username = username;
+        this.password = password;
+        this.user_id = user_id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public int getUser_id() {
+        return user_id;
+    }
+    private String password;
+    private int user_id;
+}
